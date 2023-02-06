1f2a88b493083271c95fe102089e41aba6b8f395
Guram Khasia <gurikakhasia@gmail.com>
2023-02-06 18:07:50 +0400

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import "@/styles/globals.css";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "utils/theme";
+import createEmotionCache from "utils/createEmotionCache";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+
+import type { AppProps } from "next/app";
+
+const clientSideEmotionCache = createEmotionCache();
+
+interface IApp extends AppProps {
+  emotionCache?: EmotionCache;
+}
+export default function App(props: IApp) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
